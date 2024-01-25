5e9a47abb4977bd84f7dd7838e321df7e6cef461
Aadi Umrani <47914987+aadium@users.noreply.github.com>
2024-01-25 23:39:38 +0300

    Update house_listing_tile.jsx

--- house-sale/src/components/house_listing_tile/house_listing_tile.jsx
+++ house-sale/src/components/house_listing_tile/house_listing_tile.jsx
@@ -3,10 +3,21 @@ import { useNavigate } from 'react-router-dom';
 
 function HouseListingTile({ houseAd }) {
     const navigate = useNavigate();
+    const getHouseImage = (id) => {
+        try {
+            return `https://tfhmlygvxqshejdqkmlz.supabase.co/storage/v1/object/public/house-pics/${id}/house_picture`
+        } catch (error) {
+            return 'https://via.placeholder.com/300'
+        }
+    }
     return (
         <Card style={{ border: '2px solid black', boxShadow: '0px 0px 0px 0px' }}>
             <CardContent>
-                <Typography variant="h6">Address: {houseAd.city}, {houseAd.state}, {houseAd.postal_code}</Typography>
+                <img src={getHouseImage(houseAd.id)} alt="House" style={{ width: '370px', height: '270px' }} />
+                <Typography variant="h6">{houseAd.city}, {houseAd.state}, {houseAd.postal_code}</Typography>
+                <Typography variant="body1">Bedrooms: {houseAd.bedrooms}</Typography>
+                <Typography variant="body1">Bathrooms: {houseAd.bathrooms}</Typography>
+                <Typography variant="body1">Area: {houseAd.house_area} sqft</Typography>
                 <Typography variant="body1">Price: {houseAd.price_in_usd} USD</Typography>
                 <Button variant="contained" style={{ backgroundColor: '#000000', width: '100%', marginBottom: '-10px', marginTop: '10px', boxShadow: '0px 0px 0px 0px' }} onClick={() => navigate(`/house/${houseAd.id}`)}>View</Button>
             </CardContent>
