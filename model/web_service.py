1c2739971f07b773c8b58ef6504a6335ea39c34b
Aadi Umrani <aadi@playtheory.xyz>
2024-02-11 12:01:22 +0300

    added a web service to access the model via api.

---
+++ model/web_service.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import pickle
+
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the pickled model
+with open('model/saved_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+# Define an endpoint for model inference
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get input data from the request
+    data = request.json.values()
+    float_params = [float(param) for param in data]
+    final_params = np.array(float_params).reshape(1, -1)
+
+    # Perform predictions using the loaded model
+    prediction = model.predict(final_params)
+
+    # Return the prediction as JSON
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
